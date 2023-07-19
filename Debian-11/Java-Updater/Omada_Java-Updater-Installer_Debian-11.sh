509fd3964bcf68b71e5d0319719960f7a2fb00fd
Noah0302sTech <Noah0302sTech@gmail.com>
2023-07-19 16:30:04 +0200

    Unified Initials Checks and Functions Removed Creation of Java-Update-Executer Added WGET of Java-Update-Executer Added Debug for Java-Update-Executer

--- Debian-11/Java-Updater/Omada_Java-Updater-Installer_Debian-11.sh
+++ Debian-11/Java-Updater/Omada_Java-Updater-Installer_Debian-11.sh
@@ -2,95 +2,107 @@
 #	Made by Noah0302sTech
 #	chmod +x Omada_Java-Updater-Installer_Debian-11.sh && sudo bash Omada_Java-Updater-Installer_Debian-11.sh
 
-#---------- Initial Checks & Functions
-	#----- Check for administrative privileges
-		if [[ $EUID -ne 0 ]]; then
-			echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
-			exit 1
-		fi
+#TODO:	Check if Update-Installer was executed without being called from Full-Installer
+#		If called by Full-Installer, do not move Files
+
+#---------- Initial Checks & Functions & Folder-Structure
+	#-------- Checks & Functions
+		#----- Check for administrative privileges
+			if [[ $EUID -ne 0 ]]; then
+				echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
+				exit 1
+			fi
 
 
 
-	#----- Source of Spinner-Function: https://github.com/tlatsas/bash-spinner
-			function _spinner() {
-				# $1 start/stop
-				#
-				# on start: $2 display message
-				# on stop : $2 process exit status
-				#           $3 spinner function pid (supplied from stop_spinner)
+		#----- Source of Spinner-Function: https://github.com/tlatsas/bash-spinner
+				function _spinner() {
+					# $1 start/stop
+					#
+					# on start: $2 display message
+					# on stop : $2 process exit status
+					#           $3 spinner function pid (supplied from stop_spinner)
 
-				local on_success="DONE"
-				local on_fail="FAIL"
-				local white="\e[1;37m"
-				local green="\e[1;32m"
-				local red="\e[1;31m"
-				local nc="\e[0m"
+					local on_success="DONE"
+					local on_fail="FAIL"
+					local white="\e[1;37m"
+					local green="\e[1;32m"
+					local red="\e[1;31m"
+					local nc="\e[0m"
 
-				case $1 in
-					start)
-						# calculate the column where spinner and status msg will be displayed
-						let column=$(tput cols)-${#2}-8
-						# display message and position the cursor in $column column
-						echo -ne ${2}
-						printf "%${column}s"
+					case $1 in
+						start)
+							# calculate the column where spinner and status msg will be displayed
+							let column=$(tput cols)-${#2}-8
+							# display message and position the cursor in $column column
+							echo -ne ${2}
+							printf "%${column}s"
 
-						# start spinner
-						i=1
-						sp='\|/-'
-						delay=${SPINNER_DELAY:-0.15}
+							# start spinner
+							i=1
+							sp='\|/-'
+							delay=${SPINNER_DELAY:-0.15}
 
-						while :
-						do
-							printf "\b${sp:i++%${#sp}:1}"
-							sleep $delay
-						done
-						;;
-					stop)
-						if [[ -z ${3} ]]; then
-							echo "spinner is not running.."
+							while :
+							do
+								printf "\b${sp:i++%${#sp}:1}"
+								sleep $delay
+							done
+							;;
+						stop)
+							if [[ -z ${3} ]]; then
+								echo "spinner is not running.."
+								exit 1
+							fi
+
+							kill $3 > /dev/null 2>&1
+
+							# inform the user uppon success or failure
+							echo -en "\b["
+							if [[ $2 -eq 0 ]]; then
+								echo -en "${green}${on_success}${nc}"
+							else
+								echo -en "${red}${on_fail}${nc}"
+							fi
+							echo -e "]"
+							;;
+						*)
+							echo "invalid argument, try {start/stop}"
 							exit 1
-						fi
+							;;
+					esac
+				}
 
-						kill $3 > /dev/null 2>&1
+				function start_spinner {
+					# $1 : msg to display
+					_spinner "start" "${1}" &
+					# set global spinner pid
+					_sp_pid=$!
+					disown
+				}
 
-						# inform the user uppon success or failure
-						echo -en "\b["
-						if [[ $2 -eq 0 ]]; then
-							echo -en "${green}${on_success}${nc}"
-						else
-							echo -en "${red}${on_fail}${nc}"
-						fi
-						echo -e "]"
-						;;
-					*)
-						echo "invalid argument, try {start/stop}"
-						exit 1
-						;;
-				esac
-			}
-
-			function start_spinner {
-				# $1 : msg to display
-				_spinner "start" "${1}" &
-				# set global spinner pid
-				_sp_pid=$!
-				disown
-			}
-
-			function stop_spinner {
-				# $1 : command exit status
-				_spinner "stop" $1 $_sp_pid
-				unset _sp_pid
-			}
+				function stop_spinner {
+					# $1 : command exit status
+					_spinner "stop" $1 $_sp_pid
+					unset _sp_pid
+				}
 
 
 
-	#----- Refresh Packages
-		start_spinner "Aktualisiere Package-Listen..."
-			apt update > /dev/null 2>&1
-		stop_spinner $?
-		echo
-		echo
+		#----- echoEnd
+				function echoEnd {
+					echo
+					echo
+					echo
+				}
+
+		#----- Refresh Packages
+			start_spinner "Aktualisiere Package-Listen..."
+				apt update > /dev/null 2>&1
+			stop_spinner $?
+			echoEnd
+
+
 
 	#----- Variables
 		gitURL="https://github.com/Noah0302sTech/Omada-Controller"
@@ -140,27 +152,19 @@
 
 #----- Create Java Update-Script
 	echo "----- Update-Script -----"
-	start_spinner "Erstelle Updater-Skript..."
-		#---Java-Update-Executer
-			touch /home/$SUDO_USER/$folder1Sub1File1
-		#---Cron-Check
-			touch /home/$SUDO_USER/$folder2File1
-			echo "#Init after Install" > /home/$SUDO_USER/$folder2File1
-			echo "" >> /home/$SUDO_USER/$folder2File1
-			echo "#!/bin/bash
-#	Made by Noah0302sTech
 
-#Java Update
-	echo "Update Java..."
-	"'javaUpdateOutput=$(DEBIAN_FRONTEND=noninteractive apt-get install openjdk-8-jre-headless -y 2>&1)'"
-	echo
-	echo
+	#--- Download Java-Update-Executer
+		start_spinner "Installiere Java-Updater..."
+			wget https://raw.githubusercontent.com/Noah0302sTech/Omada-Controller/master/Debian-11/Java-Updater/Executer/Omada_Java-Updater-Executer_Debian-11.sh > /dev/null 2>&1
+		stop_spinner $?
 
+	#-- Modify Debug for Java-Update-Executer
+	echo "
 #Debug
 	echo "'Java-Updater Cron-Job ran @:'" >> $folder2File1Path
 	date >> $folder2File1Path
 	echo "'$javaUpdateOutput'" >> $folder2File1Path
-	echo '' >> $folder2File1Path" > /home/$SUDO_USER/$folder1Sub1File1
+	echo '' >> $folder2File1Path" >> /home/$SUDO_USER/$folder1Sub1File1
 	stop_spinner $?
 
 	#--- Make Java-Updater.sh executable
